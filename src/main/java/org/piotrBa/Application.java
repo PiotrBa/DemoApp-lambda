86411c10a1f590aa65b778a1606afdeb4980b28c
PiotrBa <piotr.balazyk@wp.pl>
2024-07-25 12:06:50 +0100

    Code formatting

--- src/main/java/org/piotrBa/Application.java
+++ src/main/java/org/piotrBa/Application.java
@@ -4,13 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-import org.piotrBa.controller.PingController;
-
 
 @SpringBootApplication
-// We use direct @Import instead of @ComponentScan to speed up cold starts
-// @ComponentScan(basePackages = "org.piotrBa.controller")
-@Import({ PingController.class })
 public class Application {
 
     public static void main(String[] args) {
